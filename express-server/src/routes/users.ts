2ae0e857ad8e8212aa3d387154488de6c11cfcd6
Stephen Leander <stephen.leander1@gmail.com>
2022-08-11 16:04:16 -0600

    Add user GET route

--- express-server/src/routes/users.ts
+++ express-server/src/routes/users.ts
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 const user_controller = require('../controllers/user-controller');
 
 router.post('/signup', user_controller.signup_post);
 router.post('/login', user_controller.login_post);
 router.post('/logout', user_controller.logout_post);
+
+router.get('/:id', user_controller.user_get);
 
 export default router;
