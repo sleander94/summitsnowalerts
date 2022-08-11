fd230c57a6392a3d5daa71be2352828aaadd114c
Stephen Leander <stephen.leander1@gmail.com>
2022-08-11 15:40:26 -0600

    Update types & import statements

--- express-server/src/passport.ts
+++ express-server/src/passport.ts
@@ -1,7 +1,7 @@
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
+import passport from 'passport';
+import bcrypt from 'bcryptjs';
 const LocalStrategy = require('passport-local').Strategy;
-import User from './models/user';
+import User, { IUser } from './models/user';
 
 passport.use(
   new LocalStrategy(
@@ -9,15 +9,15 @@ passport.use(
       usernameField: 'email',
       passwordField: 'password',
     },
-    (email: any, password: any, done: any) => {
-      User.findOne({ email: email }, (err: any, user: any) => {
+    (email: string, password: string, done: Function) => {
+      User.findOne({ email: email }, (err: Error, user: IUser) => {
         if (err) {
           return done(err);
         }
         if (!user) {
           return done(null, false, { message: 'Incorrect username.' });
         }
-        bcrypt.compare(password, user.password, (err: any, res: any) => {
+        bcrypt.compare(password, user.password, (err: Error, res: boolean) => {
           if (res) {
             return done(null, user);
           } else {
@@ -29,12 +29,12 @@ passport.use(
   )
 );
 
-passport.serializeUser(function (user: any, done: any) {
-  done(null, user.id);
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
 
-passport.deserializeUser(function (id: any, done: any) {
-  User.findById(id, function (err: any, user: any) {
+passport.deserializeUser((id, done) => {
+  User.findById(id, function (err: Error, user: IUser) {
     done(err, user);
   });
 });
