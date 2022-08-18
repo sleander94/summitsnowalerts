0677faded0709d365e3a265e945442c2a27fb8e5
Stephen Leander <stephen.leander1@gmail.com>
2022-08-17 18:21:47 -0600

    Navigate to homepage when logged out

--- react-app/src/components/Account.tsx
+++ react-app/src/components/Account.tsx
@@ -1,3 +1,4 @@
+import { Navigate } from 'react-router-dom';
 import { AuthProps } from '../types.d';
 import AccountInfoForm from './account-actions/AccountInfoForm';
 import MountainsForm from './account-actions/MountainsForm';
@@ -5,6 +6,7 @@ import MountainsForm from './account-actions/MountainsForm';
 const Account = ({ user }: AuthProps) => {
   return (
     <section id="account">
+      {!user && <Navigate to="/" />}
       <MountainsForm user={user} />
       <AccountInfoForm user={user} />
     </section>
