7c2bd5cec7fef5a1bcab0b0369da276884bc1b80
Stephen Leander <stephen.leander1@gmail.com>
2022-08-18 15:32:35 -0600

    Add background image & login message

--- react-app/src/components/Conditions.tsx
+++ react-app/src/components/Conditions.tsx
@@ -1,25 +1,33 @@
-import { useEffect, useState } from 'react';
+import { Http2ServerRequest } from 'http2';
 import { AuthProps, mountainsObj } from '../types.d';
 import MountainWeather from './MountainWeather';
 
 const Conditions = ({ user }: AuthProps) => {
   return (
     <section id="conditions">
-      <h1>Your Mountain Forecasts</h1>
-      {user &&
-        Object.keys(user.mountains).map((key) => {
-          const mountain = key as keyof mountainsObj;
-          if (user.mountains[mountain] == 0) return;
-          console.log(mountain);
-          console.log(user.mountains[mountain]);
-          return (
-            <MountainWeather
-              key={mountain}
-              name={mountain}
-              location={user.mountains[mountain]}
-            />
-          );
-        })}
+      <div className="background-image"></div>
+      <div className="content">
+        <h1>My Mountain Forecasts</h1>
+        {!user && (
+          <h2 className="no-user">
+            Log in to view the weather at your mountains.
+          </h2>
+        )}
+        <div className="weather">
+          {user &&
+            Object.keys(user.mountains).map((key) => {
+              const mountain = key as keyof mountainsObj;
+              if (user.mountains[mountain] == undefined) return;
+              return (
+                <MountainWeather
+                  key={mountain}
+                  name={mountain}
+                  location={user.mountains[mountain] as number}
+                />
+              );
+            })}
+        </div>
+      </div>
     </section>
   );
 };
