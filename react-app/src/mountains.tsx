9128247b021b78ece6a4d3bb96f1e7c68cc944c4
Stephen Leander <stephen.leander1@gmail.com>
2022-08-18 15:27:46 -0600

    Add more Colorado mountains

--- react-app/src/mountains.tsx
+++ react-app/src/mountains.tsx
@@ -2,4 +2,13 @@ export const mountains = {
   Breckenridge: 80424,
   Keystone: 80435,
   Vail: 81657,
+  Monarch: 81227,
+  'Arapahoe Basin': 80435,
+  Copper: 80443,
+  'Winter Park': 80482,
+  Steamboat: 80487,
+  'Beaver Creek': 81620,
+  'Crested Butte': 81224,
+  Eldora: 80466,
+  Aspen: 81612,
 };
