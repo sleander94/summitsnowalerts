6fe560cd02c246d6185a3d86a6ab116cc78d2598
Stephen Leander <stephen.leander1@gmail.com>
2022-08-18 15:33:07 -0600

    Add background image

--- react-app/src/components/Home.tsx
+++ react-app/src/components/Home.tsx
@@ -4,24 +4,24 @@ import { AuthProps } from '../types.d';
 const Home = ({ user }: AuthProps) => {
   return (
     <section id="home">
-      <div className="title">
-        <h1>Summit Snow Alerts</h1>
-        <div className="background-image"></div>
+      <div className="background-image"></div>
+      <h1>Summit Snow Alerts</h1>
+      <div className="content">
+        <h2>Never miss powder at your favorite mountains</h2>
+        {user && <p>Welcome back, {user.name}.</p>}
+        {!user && (
+          <div className="link-container">
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Log In</Link>
+          </div>
+        )}
+        {user && (
+          <div className="link-container">
+            <Link to="/conditions">My Mountains</Link>
+            <Link to="/account">Edit Account</Link>
+          </div>
+        )}
       </div>
-      <h2>Never miss powder at your favorite mountains</h2>
-      {user && <p>Welcome back, {user.name}.</p>}
-      {!user && (
-        <div className="link-container">
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Log In</Link>
-        </div>
-      )}
-      {user && (
-        <div className="link-container">
-          <Link to="/conditions">My Mountains</Link>
-          <Link to="/account">Edit Profile</Link>
-        </div>
-      )}
     </section>
   );
 };
