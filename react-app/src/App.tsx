51a75e30e6b309340e904baf866b71eaa7d8f617
Stephen Leander <stephen.leander1@gmail.com>
2022-08-17 18:32:59 -0600

    Add functions to check login status & logout, update routes & render Navbar / Footer

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,23 +1,73 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Login from './components/account-actions/Login';
 import Signup from './components/account-actions/Signup';
+import Account from './components/Account';
 import Home from './components/Home';
+import Conditions from './components/Conditions';
+import Footer from './components/Footer';
+import './styles/main.css';
+import { User } from './types.d';
 
 const App = () => {
+  const [user, setUser] = useState<User>();
+
+  const getUser = (data: User) => {
+    setUser(data);
+  };
+
+  useEffect(() => {
+    console.log(user);
+    if (!user) {
+      const checkAuth = async () => {
+        try {
+          const response = await fetch('/users', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          const data = await response.json();
+          if (response.ok) setUser(data);
+          else setUser(undefined);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+      checkAuth();
+    }
+  }, [user]);
+
+  const logout = async () => {
+    try {
+      const response = await fetch('/users/logout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) setUser(undefined);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
       <Router>
+        <Navbar user={user} logout={logout} />
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home user={user} />} />
+          <Route
+            path="/login"
+            element={<Login user={user} getUser={getUser} />}
+          />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account user={user} />} />
+          <Route path="/conditions" element={<Conditions user={user} />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
