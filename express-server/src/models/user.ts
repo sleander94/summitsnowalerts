a70c7457b88f55ebbe904c6206e7c4afdad4f9b8
Stephen Leander <stephen.leander1@gmail.com>
2022-08-17 18:38:21 -0600

    Update IUser interface

--- express-server/src/models/user.ts
+++ express-server/src/models/user.ts
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 
 export interface IUser extends mongoose.Document {
-  name: { type: String; required: true };
-  email: { type: String; required: true };
-  phone: { type: String; required: false };
-  password: { type: String; required: true };
-  textAlert: { type: Boolean; required: true };
-  emailAlert: { type: Boolean; required: true };
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  textAlert: boolean;
+  emailAlert: boolean;
+  mountains: mountainsObj;
+}
+
+export interface mountainsObj {
+  Breckenridge: number;
+  Keystone: number;
+  Vail: number;
 }
 
 const UserSchema = new mongoose.Schema({
@@ -16,6 +23,7 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   textAlert: { type: Boolean, required: true },
   emailAlert: { type: Boolean, required: true },
+  mountains: { type: Object, required: true },
 });
 
 const User = mongoose.model<IUser>('User', UserSchema);
