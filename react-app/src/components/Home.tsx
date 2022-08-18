0d7ac5934182b4e0674c0587f4ceecddac36d14e
Stephen Leander <stephen.leander1@gmail.com>
2022-08-17 18:24:45 -0600

    Conditionally render welcome message & account action links

--- react-app/src/components/Home.tsx
+++ react-app/src/components/Home.tsx
@@ -1,5 +1,29 @@
-const Home = () => {
-  return <section id="home">Home</section>;
+import { Link } from 'react-router-dom';
+import { AuthProps } from '../types.d';
+
+const Home = ({ user }: AuthProps) => {
+  return (
+    <section id="home">
+      <div className="title">
+        <h1>Summit Snow Alerts</h1>
+        <div className="background-image"></div>
+      </div>
+      <h2>Never miss powder at your favorite mountains</h2>
+      {user && <p>Welcome back, {user.name}.</p>}
+      {!user && (
+        <div className="link-container">
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Log In</Link>
+        </div>
+      )}
+      {user && (
+        <div className="link-container">
+          <Link to="/conditions">My Mountains</Link>
+          <Link to="/account">Edit Profile</Link>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default Home;
