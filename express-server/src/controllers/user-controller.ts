29e709ba90bac78b0d4cd246823b2842e246e6bd
Stephen Leander <stephen.leander1@gmail.com>
2022-08-17 18:36:58 -0600

    Add auth_get, edit user_put to authenticate with password

--- express-server/src/controllers/user-controller.ts
+++ express-server/src/controllers/user-controller.ts
@@ -1,18 +1,8 @@
-import { IUser } from '@models/user';
 import { Request, Response, NextFunction } from 'express';
-import User from '../models/user';
+import User, { IUser } from '../models/user';
 import { body, validationResult } from 'express-validator';
 import passport from 'passport';
 import bcrypt from 'bcryptjs';
-
-interface UserInfo {
-  name: string;
-  email: string;
-  phone?: string;
-  password: string;
-  emailAlerts: boolean;
-  textAlerts: boolean;
-}
 
 exports.signup_post = [
   body('name', 'Enter your name.').trim().isLength({ min: 1 }).escape(),
@@ -22,7 +12,7 @@ exports.signup_post = [
     .isLength({ min: 6, max: 16 })
     .escape(),
 
-  (req: Request<UserInfo>, res: Response, next: NextFunction) => {
+  (req: Request<IUser>, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors });
@@ -40,6 +30,7 @@ exports.signup_post = [
         password: hashedPassword,
         emailAlert: req.body.emailAlert,
         textAlert: req.body.textAlert,
+        mountains: { Vail: 0, Keystone: 0, Breckenridge: 0 },
       });
       User.findOne({ email: user.email }).exec((err, results) => {
         if (err) {
@@ -49,7 +40,7 @@ exports.signup_post = [
           user.password = req.body.password;
           return res
             .status(400)
-            .json({ message: 'That username is already taken.' });
+            .json({ message: 'That email is already taken.' });
         }
         user.save((err, user) => {
           if (err) {
@@ -70,9 +61,7 @@ exports.login_post = (req: Request, res: Response, next: NextFunction) => {
       return next(err);
     }
     if (!user) {
-      return res
-        .status(401)
-        .json({ message: 'Incorrect username or password.' });
+      return res.status(401).json({ message: 'Incorrect email or password.' });
     }
     req.login(user, (err: Error) => {
       if (err) {
@@ -118,35 +107,55 @@ exports.user_put = [
     .isLength({ min: 6, max: 16 })
     .escape(),
 
-  (req: Request<UserInfo>, res: Response, next: NextFunction) => {
+  (req: Request<IUser>, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
-    if (req.isAuthenticated()) {
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ message: errors });
-      }
-      const user = new User({
-        _id: req.user._id,
-        name: req.body.name.charAt(0)
-          ? req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1)
-          : '',
-        email: req.body.email,
-        phone: req.body.phone ? req.body.phone : '',
-        password: req.body.password,
-        emailAlert: req.body.emailAlert,
-        textAlert: req.body.textAlert,
-      });
-      User.findByIdAndUpdate(req.user._id, user, {}, (err) => {
-        if (err) {
-          return next(err);
-        }
-        return res
-          .status(200)
-          .json({ message: 'Account updated successfully.' });
-      });
-    } else {
-      return res.status(401).json({
-        message: 'Access denied. Not logged in.',
-      });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors });
     }
+    passport.authenticate('local', (err: Error, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({ message: 'Incorrect password.' });
+      }
+      bcrypt.hash(
+        req.body.newPass ? req.body.newPass : req.body.password,
+        10,
+        (err: Error, hashedPassword: string) => {
+          if (err) {
+            return next(err);
+          }
+          const updatedUser = new User({
+            _id: req.user?._id,
+            name: req.body.name.charAt(0)
+              ? req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1)
+              : '',
+            email: req.body.newEmail ? req.body.newEmail : req.body.email,
+            phone: req.body.phone ? req.body.phone : '',
+            password: hashedPassword,
+            emailAlert: req.body.emailAlert,
+            textAlert: req.body.textAlert,
+            mountains: req.body.mountains,
+          });
+          User.findByIdAndUpdate(req.user?._id, updatedUser, {}, (err) => {
+            if (err) {
+              return next(err);
+            }
+            return res
+              .status(200)
+              .json({ message: 'Account updated successfully.' });
+          });
+        }
+      );
+    })(req, res, next);
   },
 ];
+
+exports.auth_get = (req: Request, res: Response, next: NextFunction) => {
+  if (req.isAuthenticated()) {
+    return res.status(200).json({ message: 'User authenticated.' });
+  } else {
+    return res.status(401).json({ message: 'Not authenticated.' });
+  }
+};
