91000ebb24a31fca7ceb3fa8e58101be542121c0
Stephen Leander <stephen.leander1@gmail.com>
2022-09-04 14:56:43 -0600

    Refactor sendAlerts function for users' day & time preferences

--- server/src/services/alerts.ts
+++ server/src/services/alerts.ts
@@ -18,128 +18,90 @@ const transporter = nodemailer.createTransport({
 });
 
 const getWeather = async (location: number) => {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_KEY}=${location}&days=2&aqi=no&alerts=no`
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_KEY}=${location}&days=2&aqi=no&alerts=no`
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-export async function sendTextAlerts() {
+export async function sendAlerts() {
+  const mountainsRef = {
+    Breckenridge: 80424,
+    Keystone: 80435,
+    Vail: 81657,
+    Monarch: 81227,
+    'Arapahoe Basin': 80435,
+    Copper: 80443,
+    'Winter Park': 80482,
+    Steamboat: 80487,
+    'Beaver Creek': 81620,
+    'Crested Butte': 81224,
+    Eldora: 80466,
+    Aspen: 81612,
+  };
   try {
-    // Get weather for all mountains
     console.log('Getting weather...');
     let weather: any = {};
-    const breckenridgeWeather = await getWeather(80424);
-    weather['Breckenridge'] = {
-      snow: breckenridgeWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        breckenridgeWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: breckenridgeWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const keystoneWeather = await getWeather(80435);
-    weather['Keystone'] = {
-      snow: keystoneWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        keystoneWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: keystoneWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const vailWeather = await getWeather(81657);
-    weather['Vail'] = {
-      snow: vailWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance: vailWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: vailWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const monarchWeather = await getWeather(81227);
-    weather['Monarch'] = {
-      snow: monarchWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        monarchWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: monarchWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const arapahoeBasinWeather = await getWeather(80435);
-    weather['Arapahoe Basin'] = {
-      snow: arapahoeBasinWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        arapahoeBasinWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: arapahoeBasinWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const copperWeather = await getWeather(80443);
-    weather['Copper'] = {
-      snow: copperWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        copperWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: copperWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const winterParkWeather = await getWeather(80482);
-    weather['Winter Park'] = {
-      snow: winterParkWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        winterParkWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: winterParkWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const steamboatWeather = await getWeather(80487);
-    weather['Steamboat'] = {
-      snow: steamboatWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        steamboatWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: steamboatWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const beaverCreekWeather = await getWeather(81620);
-    weather['Beaver Creek'] = {
-      snow: beaverCreekWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        beaverCreekWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: beaverCreekWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const crestedButteWeather = await getWeather(81224);
-    weather['Crested Butte'] = {
-      snow: crestedButteWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        crestedButteWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: crestedButteWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const eldoraWeather = await getWeather(80466);
-    weather['Eldora'] = {
-      snow: eldoraWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance:
-        eldoraWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: eldoraWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    const aspenWeather = await getWeather(81612);
-    weather['Aspen'] = {
-      snow: aspenWeather.forecast.forecastday[0].day.daily_will_it_snow,
-      snowChance: aspenWeather.forecast.forecastday[0].day.daily_chance_of_snow,
-      precip: aspenWeather.forecast.forecastday[0].day.totalprecip_in,
-    };
-    console.log('Weather ready, sending to users...');
+    await Promise.all(
+      Object.keys(mountainsRef).map(async (key) => {
+        const mountain = key as keyof mountainsObj;
+        const localWeather = await getWeather(mountainsRef[mountain]);
+        weather[mountain] = {
+          snow: localWeather.forecast.forecastday[0].day.daily_will_it_snow,
+          snowChance:
+            localWeather.forecast.forecastday[0].day.daily_chance_of_snow,
+          precip: localWeather.forecast.forecastday[0].day.totalprecip_in,
+        };
+      })
+    );
+    console.log('Weather received, sending alerts...');
 
-    // Send texts to users with mountains receiving snow
-    const results = await User.find({});
+    const days = [
+      'sunday',
+      'monday',
+      'tuesday',
+      'wednesday',
+      'thursday',
+      'friday',
+      'saturday',
+    ];
+    const date = new Date();
+    const currDay = days[date.getDay()];
+    const currTime = date.getHours();
+
+    const results = await User.find({
+      [`notifications.days.${currDay}`]: true,
+      [`notifications.times.${currTime}`]: true,
+    });
     for (const user of results) {
       if (
-        user.emailAlert === true ||
-        (user.textAlert === true && user.phone.length > 0)
+        user.notifications.email ||
+        (user.notifications.text && user.phone.length > 0)
       ) {
         let keys: (keyof mountainsObj)[] = Object.keys(
           user.mountains
         ) as (keyof mountainsObj)[];
         for (const mountain of keys) {
-          if (weather[mountain].snow == 1) {
-            if (user.textAlert === true && user.phone.length > 0) {
-              const body = `Summit Snow Alerts: Powder Alert for ${mountain} - ${weather[mountain].snowChance}% chance for ${weather[mountain].precip} inches. Reply STOP to unsubscribe.`;
+          if (weather[mountain].snow === 0) {
+            if (user.notifications.text && user.phone.length > 0) {
+              const body = `Summit Snow Alerts: Powder Alert for ${mountain}\n${weather[mountain].snowChance}% chance for ${weather[mountain].precip} inches.\nReply STOP to unsubscribe.`;
               client.messages.create({
                 body: body,
                 from: twilioNumber,
                 to: user.phone,
               });
             }
-            if (user.emailAlert === true) {
+            if (user.notifications.email === true) {
               const mailOptions = {
                 from: 'summitsnowalerts@gmail.com',
                 to: user.email,
                 subject: `Summit Snow Alerts: Powder Alert for ${mountain}`,
-                text: `Summit Snow Alerts: Powder Alert for ${mountain} - ${weather[mountain].snowChance}% chance for ${weather[mountain].precip} inches. Visit summitsnowalerts.com to unsubscribe.`,
+                text: `${mountain} - ${weather[mountain].snowChance}% chance for ${weather[mountain].precip} inches.\nVisit summitsnowalerts.com to unsubscribe.`,
               };
               transporter.sendMail(mailOptions, (error) => {
                 if (error) {
@@ -151,15 +113,16 @@ export async function sendTextAlerts() {
         }
       }
     }
-
-    client.messages.create({
-      body: 'Server is sending alerts',
-      from: twilioNumber,
-      to: myNumber,
-    });
+    if (currTime === 18) {
+      client.messages.create({
+        body: 'Server is online and sending alerts',
+        from: twilioNumber,
+        to: myNumber,
+      });
+    }
   } catch (err) {
     client.messages.create({
-      body: `Error sending snow alerts: ${err}`,
+      body: `Error sending alerts: ${err}`,
       from: twilioNumber,
       to: myNumber,
     });
