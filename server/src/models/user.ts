e5abff277d47111ad72e342b7e98f0ec79275d7c
Stephen Leander <stephen.leander1@gmail.com>
2022-09-03 21:27:12 -0600

    Add notifications object to User Schema

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -6,9 +6,78 @@ export interface IUser extends mongoose.Document {
   email: string;
   phone: string;
   password: string;
-  textAlert: boolean;
-  emailAlert: boolean;
+  notifications: {
+    text: boolean;
+    email: boolean;
+    times: timesObj;
+    days: daysObj;
+  };
   mountains: mountainsObj;
+}
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: false },
+    password: { type: String, required: true },
+    notifications: {
+      text: { type: Boolean, required: true },
+      email: { type: Boolean, required: true },
+      times: { type: Object, required: true },
+      days: {
+        sunday: { type: Boolean, required: true },
+        monday: { type: Boolean, required: true },
+        tuesday: { type: Boolean, required: true },
+        wednesday: { type: Boolean, required: true },
+        thursday: { type: Boolean, required: true },
+        friday: { type: Boolean, required: true },
+        saturday: { type: Boolean, required: true },
+      },
+    },
+    mountains: { type: Object, required: true },
+  },
+  { minimize: false }
+);
+
+const User = mongoose.model<IUser>('User', UserSchema);
+export default User;
+
+export interface timesObj {
+  0: boolean;
+  1: boolean;
+  2: boolean;
+  3: boolean;
+  4: boolean;
+  5: boolean;
+  6: boolean;
+  7: boolean;
+  8: boolean;
+  9: boolean;
+  10: boolean;
+  11: boolean;
+  12: boolean;
+  13: boolean;
+  14: boolean;
+  15: boolean;
+  16: boolean;
+  17: boolean;
+  18: boolean;
+  19: boolean;
+  20: boolean;
+  21: boolean;
+  22: boolean;
+  23: boolean;
+}
+
+export interface daysObj {
+  sunday: boolean;
+  monday: boolean;
+  tuesday: boolean;
+  wednesday: boolean;
+  thursday: boolean;
+  friday: boolean;
+  saturday: boolean;
 }
 
 export interface mountainsObj {
@@ -25,16 +94,3 @@ export interface mountainsObj {
   Eldora?: number;
   Aspen?: number;
 }
-
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: false },
-  password: { type: String, required: true },
-  textAlert: { type: Boolean, required: true },
-  emailAlert: { type: Boolean, required: true },
-  mountains: { type: Object, required: true },
-});
-
-const User = mongoose.model<IUser>('User', UserSchema);
-export default User;
