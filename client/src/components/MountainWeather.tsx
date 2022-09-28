adb1a95fd3ce72a2824f2001b785189b822dfa3c
Stephen Leander <stephen.leander1@gmail.com>
2022-09-28 16:02:48 -0600

    Multiply snow chance by 100 in state

--- client/src/components/MountainWeather.tsx
+++ client/src/components/MountainWeather.tsx
@@ -16,7 +16,7 @@ const MountainWeather = ({ name, location, completeLoading }: WeatherProps) => {
         icon: data.forecast.forecastday[0].day.condition.icon,
         avgtemp_f: data.forecast.forecastday[0].day.avgtemp_f,
         daily_chance_of_snow:
-          data.forecast.forecastday[0].day.daily_chance_of_snow,
+          data.forecast.forecastday[0].day.daily_chance_of_snow * 100,
         totalprecip_in: data.forecast.forecastday[0].day.totalprecip_in,
       },
       tomorrow: {
@@ -24,16 +24,18 @@ const MountainWeather = ({ name, location, completeLoading }: WeatherProps) => {
         icon: data.forecast.forecastday[1].day.condition.icon,
         avgtemp_f: data.forecast.forecastday[1].day.avgtemp_f,
         daily_chance_of_snow:
-          data.forecast.forecastday[1].day.daily_chance_of_snow,
+          data.forecast.forecastday[1].day.daily_chance_of_snow * 100,
         totalprecip_in: data.forecast.forecastday[1].day.totalprecip_in,
       },
     });
     setContent(true);
     completeLoading();
   };
+
   useEffect(() => {
     getConditions(location);
   }, [location]);
+
   return (
     <div id={name} className="mountain-weather">
       {content && (
